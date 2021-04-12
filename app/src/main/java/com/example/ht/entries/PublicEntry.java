9b5c9f930bac32f7bf855e416b1ab621a974dc39
tikka <tikkanen.kuura@gmail.com>
2021-04-12 13:12:24 +0300

    xml response

--- app/src/main/java/com/example/ht/entries/PublicEntry.java
+++ app/src/main/java/com/example/ht/entries/PublicEntry.java
@@ -1,14 +1,24 @@
 package com.example.ht.entries;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class PublicEntry extends Entry{
     Integer trainkm, buskm;
     String entryID;
     Date date;
-    Integer totalCO;
+    Integer totalCO, busCO, trainCO;
 
     public PublicEntry(String x, Integer a, Integer b) {
         entryID = x;
@@ -24,10 +34,37 @@ public class PublicEntry extends Entry{
         // url example https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/TransportCalculator/PublicTransportEstimate?longDistanceBusYear=20&longDistanceTrainYear=20&metroweek=0&tramWeek=0&cityBusWeek=0&cityTrainWeek=0
 
         String url = "https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/TransportCalculator/PublicTransportEstimate?";
-        //open travelvalues and add to url
+
+        //TODO open travelvalues and add to url
+
+        url = url + "longDistanceBusYear=" + 20 + "&longDistanceTrainYear=" + 20 + "&metroweek=0&tramWeek=0&cityBusWeek=0&cityTrainWeek=0";
 
         // send request
-
-        // response -> save totalCO
+        readXML(url);
     }
+
+    public void readXML(String url) {
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+            // get response document
+            Document doc1 = builder.parse(url);
+
+            System.out.println(doc1.getDocumentElement().getNodeName());
+
+            busCO = Integer.parseInt(doc1.getDocumentElement().getAttributeNode("Bus").getValue());
+            trainCO = Integer.parseInt(doc1.getDocumentElement().getAttributeNode("Train").getValue());
+            totalCO = Integer.parseInt(doc1.getDocumentElement().getAttributeNode("Total").getValue());
+
+            System.out.println(busCO + ", " + trainCO + ", " + totalCO);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
